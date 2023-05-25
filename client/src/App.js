c6155ba05848f96641b77638eda81c9607e407aa
Sara <Sara.Ginsburg@flatironschool.com>
2023-05-24 21:54:39 -0400

    navbar created and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className='App'>
-      <h1>bookStand</h1>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main></main>
+      </Router>
+    </ChakraProvider>
   );
 }
 
